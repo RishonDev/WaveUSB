d1580ce8f0e4dbcf99b2d048a77edb32e834cec2
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-07-20 16:05:28 +0530

    many items added

--- src/WaveUSB/Main.java
+++ src/WaveUSB/Main.java
@@ -3,7 +3,7 @@ package WaveUSB;
 import javax.swing.*;
 import java.awt.*;
 
-class Main{
+class Main implements Constants{
     static JFrame frame = new JFrame();
     static CardLayout layout = new CardLayout();
     static JFileChooser jFileChooser = new JFileChooser();
